9982eaf68a721f6b7d7eab4f311fc9a07c2fd818
dkibogy <jepkemboikibogy5@gmail.com>
2020-06-15 01:00:51 +0300

    program code template for form in the quotes class

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,13 @@
-export class Quotes {
-}
+export class Quote {
+    publishQuote: boolean;
+  
+    constructor(
+      public category: string,
+      public author: string,
+      public message: string,
+      public date: Date,
+      
+    ) {
+      this.publishQuote = false;
+    }
+  }
