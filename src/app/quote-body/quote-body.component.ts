40ea330ddd59b0445efdc3b72b8753ee7bd468b1
dkibogy <jepkemboikibogy5@gmail.com>
2020-06-15 11:21:21 +0300

    add a quote bank with quote elements defined

--- src/app/quote-body/quote-body.component.ts
+++ src/app/quote-body/quote-body.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote} from '../quotes'
 
 @Component({
   selector: 'app-quote-body',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-body.component.css']
 })
 export class QuoteBodyComponent implements OnInit {
+  quotes = [
+    new Quote('Tech', "steve Jobs","Innovation distinguishes between a leader and a follower.", 'suzie' ),
+    new Quote('love ','Dalai Lama',"love is the absense of judgement", "daisy"),
+    new Quote('life', 'Oprah Winfrey', 'Think like a Queen. A queen who is not afraid to fail', 'jane')
+  ]
 
   constructor() { }
 
